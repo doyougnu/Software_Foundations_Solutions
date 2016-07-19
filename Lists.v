1902a06edf5a8fc3bd3f1cf8202d6e59d5900c2c
doyougnu <jmy6342@gmail.com>
2016-07-19 15:55:31 -0500

    finished alternate

--- Lists.v
+++ Lists.v
@@ -307,17 +307,24 @@ Proof. simpl. reflexivity. Qed.
 
 
 Fixpoint alternate (l1 l2 : natlist) : natlist :=
-  (* FILL IN HERE *) admit.
+  match l1 with
+  | nil => l2
+  | h1 :: nil => h1 :: l2
+  | h1 :: t1 => match l2 with
+                | nil => l1
+                | h2 :: t2 =>  (h1 :: h2 :: (alternate t1 t2))
+                end
+end.
 
 
 Example test_alternate1:        alternate [1;2;3] [4;5;6] = [1;4;2;5;3;6].
- (* FILL IN HERE *) Admitted.
+Proof. simpl. reflexivity. Qed.
 Example test_alternate2:        alternate [1] [4;5;6] = [1;4;5;6].
- (* FILL IN HERE *) Admitted.
+Proof. simpl. reflexivity. Qed.
 Example test_alternate3:        alternate [1;2;3] [4] = [1;4;2;3].
- (* FILL IN HERE *) Admitted.
+Proof. simpl. reflexivity. Qed.
 Example test_alternate4:        alternate [] [20;30] = [20;30].
- (* FILL IN HERE *) Admitted. 
+Proof. simpl. reflexivity. Qed.
 (** [] *)
 
 (* ###################################################### *)
