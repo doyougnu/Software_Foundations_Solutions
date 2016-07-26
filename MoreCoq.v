6d93dbfa072dae83e71f43ee26bc6c05d6c37f22
doyougnu <jmy6342@gmail.com>
2016-07-25 21:59:23 -0500

    finished beq_nat_true

--- MoreCoq.v
+++ MoreCoq.v
@@ -541,7 +541,13 @@ Proof.
 Theorem beq_nat_true : forall n m,
     beq_nat n m = true -> n = m.
 Proof.
-  (* FILL IN HERE *) Admitted.
+  intros n. induction n as [| n'].
+  Case "n = 0". intros m. destruct m as [| m']. reflexivity.
+  SCase "m = S m'". intros eq. inversion eq.
+  Case "n = S n'". intros m eq. destruct m as [| m'].
+    SCase "m = 0". inversion eq.
+    SCase "m = S m'". apply f_equal. rewrite <- IHn'. reflexivity. 
+    inversion eq. reflexivity. Qed.
 (** [] *)
 
 (** **** Exercise: 2 stars, advanced (beq_nat_true_informal)  *)
