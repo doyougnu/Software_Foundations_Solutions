5277460a162d7db9c68ae8a576e35708fc158b05
doyougnu <jmy6342@gmail.com>
2016-07-26 13:54:48 -0500

    skipped double induction

--- MoreCoq.v
+++ MoreCoq.v
@@ -795,17 +795,16 @@ Proof.
 
 (** **** Exercise: 3 stars, optional (double_induction)  *)
 (** Prove the following principle of induction over two naturals. *)
-
+(*
 Theorem double_induction: forall (P : nat -> nat -> Prop), 
   P 0 0 ->
   (forall m, P m 0 -> P (S m) 0) ->
   (forall n, P 0 n -> P 0 (S n)) ->
   (forall m n, P m n -> P (S m) (S n)) ->
   forall m n, P m n.
-Proof.
-  (* FILL IN HERE *) Admitted.
+                            (* skipping*) 
 (** [] *)
-
+*)
 
 (* ###################################################### *)
 (** * Using [destruct] on Compound Expressions *)
